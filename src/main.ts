117c08038dbf7412e0fed9904ab5f330b4d196c1
Gabriel Fiocchi <gabriel.fiocchi@crealogix.com>
2022-07-12 17:36:33 +0200

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Pokeapi example')
+    .setDescription('The pokemon API description')
+    .setVersion('1.0')
+    .addTag('pokemons')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+
   app.enableCors();
   const host = process.env.host || 'localhost';
   const port = process.env.PORT || 3001;
